a8c450bb7edd23b27bfead78c30f1ba817a1124b
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-06 23:57:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,13 @@
-def my_own_map(source_array) 
+def my_own_map(source_array)
+
+  new_array = []
+  counter = 0
+
+  while counter < source_array.length do
+    new_array << source_array[counter]*-1
+    counter += 1
+  end
+
+  return new_array
   
 end
