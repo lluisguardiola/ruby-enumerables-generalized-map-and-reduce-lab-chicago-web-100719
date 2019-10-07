daf087ebde33c2eb20c5db16f6f48de979babdbc
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-07 00:26:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,7 @@ def map(source_array)
   return new_array
 end
 
-def reduce(source_array, starting_point=0)
+def reduce(source_array, starting_point = nil)
   
   if starting_point
     value_to_return = starting_point
